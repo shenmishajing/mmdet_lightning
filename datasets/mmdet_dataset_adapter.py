8352e783502e3ae50e7f0954eb59f781fb6812c1
shenmishajing <shenmishajing@gmail.com>
2023-02-27 15:31:54 +0800

    add mmdetection feature

---
+++ datasets/mmdet_dataset_adapter.py
@@ -0,0 +1,67 @@
+from functools import partial
+from typing import Sequence
+
+from lightning.pytorch.cli import instantiate_class
+from mmdet.datasets import AspectRatioBatchSampler as _AspectRatioBatchSampler
+from mmengine.dataset import COLLATE_FUNCTIONS
+
+from datasets.base import LightningDataModule
+
+
+class MMDetDataSetAdapter(LightningDataModule):
+    def __init__(self, evaluator_cfg: dict, visualizer_cfg: dict, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.evaluator_cfg = self.get_split_config(evaluator_cfg)
+        self.visualizer_cfg = self.get_split_config(visualizer_cfg)
+
+        self.evaluators = {}
+        self.visualizers = {}
+
+    def setup(self, stage=None):
+        super().setup(stage)
+        split_names = self._get_split_names(stage)
+
+        for name in split_names:
+            self.evaluators[name] = instantiate_class(tuple(), self.evaluator_cfg[name])
+            self.visualizers[name] = instantiate_class(
+                tuple(), self.visualizer_cfg[name]
+            )
+
+            if hasattr(self.datasets[name], "metainfo"):
+                self.evaluators[name].dataset_meta = self.datasets[name].metainfo
+                self.visualizers[name].dataset_meta = self.datasets[name].metainfo
+
+    def _build_collate_fn(self, collate_fn_cfg):
+        # The default behavior of `collat_fn` in dataloader is to
+        # merge a list of samples to form a mini-batch of Tensor(s).
+        # However, in mmengine, if `collate_fn` is not defined in
+        # dataloader_cfg, `pseudo_collate` will only convert the list of
+        # samples into a dict without stacking the batch tensor.
+        collate_fn_type = collate_fn_cfg.pop("type")
+        return partial(COLLATE_FUNCTIONS.get(collate_fn_type), **collate_fn_cfg)  # type: ignore
+
+
+class AspectRatioBatchSampler(_AspectRatioBatchSampler):
+    def __iter__(self) -> Sequence[int]:
+        for idx in self.sampler:
+            data_info = self.sampler.dataset._sampler.data_source.get_data_info(idx)
+            width, height = data_info["width"], data_info["height"]
+            bucket_id = 0 if width < height else 1
+            bucket = self._aspect_ratio_buckets[bucket_id]
+            bucket.append(idx)
+            # yield a batch of indices in the same aspect ratio group
+            if len(bucket) == self.batch_size:
+                yield bucket[:]
+                del bucket[:]
+
+        # yield the rest data and reset the bucket
+        left_data = self._aspect_ratio_buckets[0] + self._aspect_ratio_buckets[1]
+        self._aspect_ratio_buckets = [[] for _ in range(2)]
+        while len(left_data) > 0:
+            if len(left_data) <= self.batch_size:
+                if not self.drop_last:
+                    yield left_data[:]
+                left_data = []
+            else:
+                yield left_data[: self.batch_size]
+                left_data = left_data[self.batch_size :]
---
+++ models/mmdet_model_adapter.py
@@ -0,0 +1,146 @@
+import os
+import shutil
+from abc import ABC
+from types import MethodType
+
+import cv2
+import mmcv
+import torch
+from torch import nn
+
+from .base import LightningModule
+
+
+class MMDetModelAdapter(LightningModule, ABC):
+    """Lightning module specialized for EfficientDet, with metrics support.
+
+    The methods `forward`, `training_step`, `validation_step`, `validation_epoch_end`
+    are already overriden.
+
+    # Arguments
+        model: The pytorch model to use.
+        metrics: `Sequence` of metrics to use.
+
+    # Returns
+        A `LightningModule`.
+    """
+
+    def __init__(
+        self,
+        model: nn.Module,
+        visualizer_kwargs=None,
+        *args,
+        **kwargs,
+    ):
+        """
+        To show a metric in the progressbar a list of tupels can be provided for metrics_log_info, the first
+        entry has to be the name of the metric to log and the second entry the display name in the progressbar. By default the
+        mAP is logged to the progressbar.
+        """
+        super().__init__(*args, **kwargs)
+        # self._output_paths = ["cam", "result"]
+        self._output_paths = ["result"]
+
+        self.model = model
+        self.model.data_preprocessor.cast_data = MethodType(
+            lambda obj, data: data, self.model.data_preprocessor
+        )
+
+        if visualizer_kwargs is None:
+            self.visualizer_kwargs = {}
+        else:
+            self.visualizer_kwargs = visualizer_kwargs
+
+    def forward(self, batch, mode="loss"):
+        self.batch_size = len(batch["inputs"])
+        batch = self.model.data_preprocessor(batch, mode != "predict")
+        return self.model._run_forward(batch, mode=mode)
+
+    def forward_step(self, batch, split="val"):
+        outputs = self(batch, mode="predict")
+        self.trainer.datamodule.evaluators[split].process(outputs, batch)
+        return outputs
+
+    def forward_epoch_end(self, split="val"):
+        log_vars = self.trainer.datamodule.evaluators[split].evaluate(
+            len(self.trainer.datamodule.datasets[split])
+        )
+        self.log_dict(self.add_prefix(log_vars, split), sync_dist=True)
+        return log_vars
+
+    def training_step(self, batch, batch_idx):
+        outputs = self(batch)
+        _, log_vars = self.model.parse_losses(outputs)
+        self.log_dict(self.add_prefix(log_vars))
+        return log_vars
+
+    def validation_step(self, batch, *args, **kwargs):
+        return self.forward_step(batch)
+
+    def validation_epoch_end(self, *args, **kwargs):
+        return self.forward_epoch_end()
+
+    def test_step(self, batch, *args, **kwargs):
+        return self.forward_step(batch, "test")
+
+    def test_epoch_end(self, *args, **kwargs):
+        return self.forward_epoch_end("test")
+
+    def cam_visualization(self, batch, *args, **kwargs):
+        batch = self.model.data_preprocessor(batch)
+
+        if isinstance(batch, dict):
+            inputs = batch["inputs"]
+            data_samples = batch["data_samples"]
+        elif isinstance(batch, (list, tuple)):
+            inputs, data_samples = batch
+
+        outputs = [
+            nn.functional.interpolate(
+                output.mean(dim=1, keepdim=True),
+                size=data_samples[0].batch_input_shape,
+                mode="bilinear",
+            )
+            for output in self.model.extract_feat(inputs)
+        ]
+        outputs = [
+            ((output - output.min()) / (output.max() - output.min(output)) * 255 + 0.5)
+            .clamp_(0, 255)
+            .permute(0, 2, 3, 1)
+            .to("cpu", torch.uint8)
+            .numpy()
+            for output in outputs
+        ]
+
+        for i, data_sample in enumerate(data_samples):
+            name = os.path.basename(data_sample.img_path)
+            for layer_num, output in enumerate(outputs):
+                mmcv.imwrite(
+                    cv2.applyColorMap(output[i], cv2.COLORMAP_JET),
+                    os.path.join(
+                        self.cam_output_path,
+                        os.path.splitext(name)[0] + f"_{layer_num}.png",
+                    ),
+                )
+            shutil.copy2(
+                data_sample.img_path,
+                os.path.join(self.cam_output_path, name),
+            )
+
+    def result_visualization(self, batch, *args, **kwargs):
+        for output in self(batch, mode="predict"):
+            # rescale gt bboxes
+            assert output.get("scale_factor") is not None
+            output.gt_instances.bboxes /= output.gt_instances.bboxes.new_tensor(
+                output.scale_factor
+            ).repeat((1, 2))
+
+            # result visualization
+            name = os.path.basename(output.img_path)
+            self.trainer.datamodule.visualizers["predict"].add_datasample(
+                name,
+                mmcv.imread(output.img_path, channel_order="rgb"),
+                output,
+                out_file=os.path.join(self.result_output_path, name),
+                **self.visualizer_kwargs,
+            )
