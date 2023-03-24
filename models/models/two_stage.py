bc363cc98971f254ddf02c51a61021d74bdcfce6
shenmishajing <shenmishajing@gmail.com>
2023-03-24 19:43:28 +0800

    move two stage model to models.models

--- models/models/two_stage.py
+++ models/models/two_stage.py
@@ -5,7 +5,6 @@ from mmengine.config import ConfigDict
 class TwoStageDetector(_TwoStageDetector):
     def __init__(self, *args, **kwargs) -> None:
         for k, v in kwargs.items():
-            if not isinstance(v, ConfigDict):
+            if isinstance(v, dict):
                 kwargs[k] = ConfigDict(v)
-        args = [ConfigDict(arg) for arg in args]
-        super().__init__(*args, **kwargs)
+        super().__init__(**kwargs)
