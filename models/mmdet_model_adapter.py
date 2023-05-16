0544a96ae75027e111be9ab6fb827a38eaaca931
shenmishajing <shenmishajing@gmail.com>
2023-05-16 17:43:46 +0800

    remove predict cam method

--- models/mmdet_model_adapter.py
+++ models/mmdet_model_adapter.py
@@ -1,7 +1,5 @@
 import os
-import shutil
 
-import cv2
 import mmcv
 from mmlab_lightning.models import MMLabModelAdapter
 
@@ -9,7 +7,7 @@ from mmlab_lightning.models import MMLabModelAdapter
 class MMDetModelAdapter(MMLabModelAdapter):
     def __init__(self, predict_tasks=None, *args, **kwargs):
         if predict_tasks is None:
-            predict_tasks = ["cam", "result"]
+            predict_tasks = ["result"]
         super().__init__(*args, predict_tasks=predict_tasks, **kwargs)
 
     def predict_forward(self, batch, *args, **kwargs):
@@ -21,24 +19,6 @@ class MMDetModelAdapter(MMLabModelAdapter):
                 output.scale_factor
             ).repeat((1, 2))
         return predict_result
-
-    def predict_cam(
-        self, *args, data_samples, feature_map_outputs, output_path, **kwargs
-    ):
-        for i, data_sample in enumerate(data_samples):
-            name = os.path.basename(data_sample.img_path)
-            for layer_num, output in enumerate(feature_map_outputs):
-                mmcv.imwrite(
-                    cv2.applyColorMap(output[i], cv2.COLORMAP_JET),
-                    os.path.join(
-                        output_path,
-                        os.path.splitext(name)[0] + f"_{layer_num}.png",
-                    ),
-                )
-            shutil.copy2(
-                data_sample.img_path,
-                os.path.join(output_path, name),
-            )
 
     def predict_result(self, *args, predict_outputs, output_path, **kwargs):
         for output in predict_outputs:
